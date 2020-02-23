7d101fcd00597692c9c8ebb927b6811cf9e85346
nelson tejeda <nelsonjesustejeda@gmail.com>
2020-02-23 14:27:07 -0500

    added navbar component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>navbar</div>
+      <div>
+        <Navbar />
+      </div>
       <Route path="/" exact component={Home} />
       <Route path="/about" exact component={About} />
     </BrowserRouter>
