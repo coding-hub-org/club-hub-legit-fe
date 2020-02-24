99f3a941b4a87dd997b228ae051538ea0a95e510
nelson tejeda <nelsonjesustejeda@gmail.com>
2020-02-24 00:50:06 -0500

    Made routes to switch and added login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -11,8 +12,11 @@ function App() {
       <div>
         <Navbar />
       </div>
-      <Route path="/" exact component={Home} />
-      <Route path="/about" exact component={About} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/login" component={Login} />
+      </Switch>
     </BrowserRouter>
   );
 }
