24f345257ace96e8d5cd294723f8e4c70e73f8c9
Noah Lambert <nl111799@gmail.com>
2020-03-05 18:58:50 -0500

    set up homepage framework

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,32 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { loginStatus } from "../actions";
+import "../Home.css";
 
 function Home() {
   //pulling our reducer from anywhere.
   //dummy example. will be changed later.
   const login = useSelector(state => state.login);
   const dispatch = useDispatch();
+  
   return (
     <div>
-      <h1>hello world. Login status: {login.toString()}</h1>
-      <button onClick={() => dispatch(loginStatus())}>login/logout</button>
+      <div className='feat-clubs-section'>
+        <h3 id='featured-clubs'>Featured Clubs of the week</h3>
+        <p id='featured-clubs-text'>Top clubs based on attendance and reviews last week</p>
+      </div>
+
+      <div className='category-section'>
+        <h3 id='categories'>Categories</h3>
+        <button id='science'>Science</button>
+        <button id='cultural'>Cultural</button>
+        <button id='self-dev'>Self-Development</button>
+      </div>
+
+      <div className='all-clubs-section'>
+        <h3 id='all-clubs'>All Clubs</h3>
+      </div>
+      
     </div>
   );
 }
