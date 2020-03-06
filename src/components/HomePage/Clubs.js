6ccf3cc66030eaa2236ca7df8d1e745386d85a5f
nelson tejeda <nelsonjesustejeda@gmail.com>
2020-03-05 20:00:20 -0500

    added club component to home

---
+++ src/components/HomePage/Clubs.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Clubs() {
+  return <div>fejbnjkfbskjbfskj</div>;
+}
+
+export default Clubs;
--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -2,31 +2,34 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 //import { loginStatus } from "../actions";
 import "./Home.css";
+import Clubs from "./Clubs";
 
 function Home() {
   //pulling our reducer from anywhere.
   //dummy example. will be changed later.
-  const login = useSelector(state => state.login);
-  const dispatch = useDispatch();
-  
+  //const login = useSelector(state => state.login);
+  //const dispatch = useDispatch();
+
   return (
     <div>
-      <div className='feat-clubs-section'>
-        <h3 id='featured-clubs'>Featured Clubs of the week</h3>
-        <p id='featured-clubs-text'>Top clubs based on attendance and reviews last week</p>
+      <div className="feat-clubs-section">
+        <h3 id="featured-clubs">Featured Clubs of the week</h3>
+        <p id="featured-clubs-text">
+          Top clubs based on attendance and reviews last week
+        </p>
       </div>
 
-      <div className='category-section'>
-        <h3 id='categories'>Categories</h3>
-        <button id='science'>Science</button>
-        <button id='cultural'>Cultural</button>
-        <button id='self-dev'>Self-Development</button>
+      <div className="category-section">
+        <h3 id="categories">Categories</h3>
+        <button id="science">Science</button>
+        <button id="cultural">Cultural</button>
+        <button id="self-dev">Self-Development</button>
       </div>
 
-      <div className='all-clubs-section'>
-        <h3 id='all-clubs'>All Clubs</h3>
+      <div className="all-clubs-section">
+        <h3 id="all-clubs">All Clubs</h3>
+        <Clubs />
       </div>
-      
     </div>
   );
 }
