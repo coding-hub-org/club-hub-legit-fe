f51c6adfbc1449bcb946b65efdfe508d3111fa78
nelson tejeda <nelsonjesustejeda@gmail.com>
2020-03-06 00:54:12 -0500

    turned into class based component to control prop flow

--- src/components/HomePage/Clubs.js
+++ src/components/HomePage/Clubs.js
@@ -1,22 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Clubs.css";
 
-function Clubs() {
-  return (
-      <div> 
-        <div className="club-img">
-          *img*
-        </div>
+class Clubs extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <div className="club-img">*img*</div>
 
-        <p id='p1'>
-          *placeholder1*
-        </p>
+        <p id="p1">*placeholder1*</p>
 
-        <p id='p2'>
-          *placeholder2*
-        </p>
+        <p id="p2">*placeholder2*</p>
       </div>
-  );  
+    );
+  }
 }
 
 export default Clubs;
