7ab86b8f0592283bc217bfe6f1d3994cfc812096
nelson tejeda <nelsonjesustejeda@gmail.com>
2020-03-06 10:28:46 -0500

    chaned route for club so it correlates to the given id

--- src/components/ClubPage/ClubInfo.js
+++ src/components/ClubPage/ClubInfo.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 
-function ClubInfo() {
-  return (
-    <div>
-      <h1>name of club</h1>
-      <h3>photo of club</h3>
-      <p>some sort of info on the club</p>
-    </div>
-  );
+class ClubInfo extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <h1>name of club {this.props.match.params.id}</h1>
+        <h3>photo of club</h3>
+        <p>some sort of info on the club</p>
+      </div>
+    );
+  }
 }
 
 export default ClubInfo;
